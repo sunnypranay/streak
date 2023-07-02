e7c87e1ddb9aebc70f58a53bb6dd499562815928
Sunny Pranay <sunnypranay@Sunnys-MacBook-Air.local>
2023-07-01 21:24:10 -0500

    date

---
+++ randomizer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Generate a random number between 1 and 24
+RUN_TIMES=$(( RANDOM % 24 + 1 ))
+
+# Run the script the random number of times
+for (( i=0; i<$RUN_TIMES; i++ ))
+do
+  /home/titan/Desktop/Projects/streak/script.sh >> /home/titan/Desktop/Projects/streak/test.log 2>&1
+  # Sleep for a random interval to spread the runs out over the day
+  sleep $(( RANDOM % 3600 ))
+done
+
