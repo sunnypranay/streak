7cd3ed18f120fac030f9b7300eaced70daeb05a5
sunnypranay <mpranay2017@gmail.com>
2023-07-01 22:40:37 -0500

    date

--- randomizer.sh
+++ randomizer.sh
@@ -6,7 +6,7 @@ RUN_TIMES=$(( RANDOM % 24 + 1 ))
 # Run the script the random number of times
 for (( i=0; i<$RUN_TIMES; i++ ))
 do
-  /home/titan/Desktop/Projects/streak/script.sh >> /home/titan/Desktop/Projects/streak/test.log 2>&1
+  /home/titan/Desktop/projects/streak/script.sh >> /home/titan/Desktop/projects/streak/test.log 2>&1
   # Sleep for a random interval to spread the runs out over the day
   sleep $(( RANDOM % 3600 ))
 done
